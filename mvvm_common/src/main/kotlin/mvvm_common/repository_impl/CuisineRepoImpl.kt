2092f14b09296c37cc18f86cda55941beb84835d
Dev Kumar <erkumardevender@gmail.com>
2023-08-19 23:42:27 +0530

    So finally we wanted to use API from retrofit API but got failed even after adding some lines of code in CuisineRepoImpl class. It required context that we aren' supposed to pass to any other  module except presentation or app.
    we also commented the code in CuisineRepoImpl class that was getting the data simply from a plain text.
    
    Before proceeding any further we encountered erro and we are stopping here. In next step we will use HILT's DI, to make it work via retrofit

--- mvvm_common/src/main/kotlin/mvvm_common/repository_impl/CuisineRepoImpl.kt
+++ mvvm_common/src/main/kotlin/mvvm_common/repository_impl/CuisineRepoImpl.kt
@@ -2,15 +2,27 @@ package mvvm_common.repository_impl
 
 import com.google.gson.Gson
 import com.sanjoo.mvvm_data.models.PlainTextDataProvider
+import com.sanjoo.mvvm_data.retrofit.ApiServiceClass
+import com.sanjoo.mvvm_data.retrofit.RetrofitClass
 import com.sanjoo.mvvm_domain.entities.CuisineEntity
 import com.sanjoo.mvvm_domain.entities.RestaurantInfoEntity
 import com.sanjoo.mvvm_domain.repo_interface.CuisinesRepo
 
 class CuisineRepoImpl:CuisinesRepo {
     override fun getAllCuisines(): List<CuisineEntity> {
-         val restaurantInfoEntity:RestaurantInfoEntity =
+         /*val restaurantInfoEntity:RestaurantInfoEntity =
              Gson().fromJson(PlainTextDataProvider.allCuisinesAvailableJsonData,RestaurantInfoEntity::class.java)
-        return restaurantInfoEntity.cuisines
+        return restaurantInfoEntity.cuisines*/
+
+        //now here is the problem, we want to use ApiService class but that will be taking context(maybe other things as well) in params. Take a look
+    // at below code
+        val retrofitClass=RetrofitClass()
+        val okHttpClient=RetrofitClass().getOKHttpClient()
+        val retrofitBuilder=RetrofitClass().getRetrofitBuilder(okHttpClient)
+
+        val apiServiceClass=ApiServiceClass()
+
+        apiServiceClass.getApiService(retrofitClass.getRetrofitBuilder(okHttpClient))
     }
 
 }
